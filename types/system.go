5115bf72e1864e0a1eac31141f1ec6ab20304301
link <a624669980@163.com>
2022-02-17 18:44:51 +0800

    update desc

--- types/system.go
+++ types/system.go
@@ -2,4 +2,4 @@ package types
 
 const CURRENTVERSION = "0.2.9"
 
-const BODY = "<li>Custom installation of new parameters</li><li>Fixed issues</li>"
+const BODY = "<li>Custom installation of new parameters</li><li>Fixed issues</li><li>Update front-end translation</li>"
