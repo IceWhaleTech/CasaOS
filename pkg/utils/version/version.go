3f53e6f33be94cdf0139358cd9446f38c0ba2a11
link <a624669980@163.com>
2022-06-14 13:24:53 +0800

    Dev (#288)
    * fix bug
    
    * updata UI
    
    * 0.3.2
    
    ### Added
    
    - [Files] Files can now be selected multiple files and downloaded, deleted, moved, etc.
    - [Apps] Support to modify the application opening address.([#204](https://github.com/IceWhaleTech/CasaOS/issues/204))
    
    ### Changed
    
    - [Apps] Hide the display of non-essential environment variables in the application.
    - [System] Network, disk, cpu, memory, etc. information is modified to be pushed via socket.
    - [System] Optimize opening speed.([#214](https://github.com/IceWhaleTech/CasaOS/issues/214))
    ### Fixed
    
    - [System] Fixed the problem that sync data cannot submit the device ID ([#68](https://github.com/IceWhaleTech/CasaOS/issues/68))
    - [Files] Fixed the code editor center alignment display problem.([#210](https://github.com/IceWhaleTech/CasaOS/issues/210))
    - [Files] Fixed the problem of wrong name when downloading files.([#240](https://github.com/IceWhaleTech/CasaOS/issues/240))
    - [System] Fixed the network display as a negative number problem.([#224](https://github.com/IceWhaleTech/CasaOS/issues/224))
    
    * Modify log help class
    
    * Fix some bugs in 0.3.2
    
    * Solve the operation file queue problem
    
    * Exclude web folders
    
    * update UI
    
    * add cancel file operate
    
    * Update UI
    
    * Merge sockets to transfer data
    
    * Conflict Resolution
    
    * Update send data interval
    
    * Update UI
    
    * fixed bug
    
    - Fix the problem of application opening failure on non-80 ports
    - Modify port failure problem
    - Modify environment variables disappearing problem
    
    * update version function
    
    * Fix version update issues

--- pkg/utils/version/version.go
+++ pkg/utils/version/version.go
@@ -2,7 +2,7 @@
  * @Author: LinkLeong link@icewhale.com
  * @Date: 2022-05-13 18:15:46
  * @LastEditors: LinkLeong
- * @LastEditTime: 2022-06-02 17:41:47
+ * @LastEditTime: 2022-06-14 11:17:16
  * @FilePath: /CasaOS/pkg/utils/version/version.go
  * @Description:
  * @Website: https://www.casaos.io
@@ -29,6 +29,7 @@ func IsNeedUpdate() (bool, model.Version) {
 	json2.Unmarshal([]byte(data.String()), &version)
 
 	v1 := strings.Split(version.Version, ".")
+
 	v2 := strings.Split(types.CURRENTVERSION, ".")
 
 	for len(v1) < len(v2) {
@@ -42,29 +43,7 @@ func IsNeedUpdate() (bool, model.Version) {
 		b, _ := strconv.Atoi(v2[i])
 		if a > b {
 			return true, version
-		} else {
-			return false, version
 		}
 	}
 	return false, version
-}
-
-//a版本大于b版本
-func VersionCompared(a string, b string) bool {
-	v1 := strings.Split(a, ".")
-	v2 := strings.Split(b, ".")
-	for len(v1) < len(v2) {
-		v1 = append(v1, "0")
-	}
-	for len(v2) < len(v1) {
-		v2 = append(v2, "0")
-	}
-	for i := 0; i < len(v1); i++ {
-		a, _ := strconv.Atoi(v1[i])
-		b, _ := strconv.Atoi(v2[i])
-		if a > b {
-			return true
-		}
-	}
-	return false
 }
--- types/system.go
+++ types/system.go
@@ -2,7 +2,7 @@
  * @Author: LinkLeong link@icewhale.com
  * @Date: 2022-02-17 18:53:22
  * @LastEditors: LinkLeong
- * @LastEditTime: 2022-06-13 19:24:15
+ * @LastEditTime: 2022-06-14 08:58:14
  * @FilePath: /CasaOS/types/system.go
  * @Description:
  * @Website: https://www.casaos.io
