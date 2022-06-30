42dda275b6d4905589539395f5caf62514d001cc
a624669980@163.com <a624669980@163.com>
2022-06-30 16:18:57 +0800

    Update assist.sh

--- shell/assist.sh
+++ shell/assist.sh
@@ -3,11 +3,11 @@
  # @Author: LinkLeong link@icewhale.com
  # @Date: 2022-02-17 18:53:29
  # @LastEditors: LinkLeong
- # @LastEditTime: 2022-06-30 16:10:54
+ # @LastEditTime: 2022-06-30 16:18:32
  # @FilePath: /CasaOS/shell/assist.sh
  # @Description: 
  # @Website: https://www.casaos.io
  # Copyright (c) 2022 by icewhale, All Rights Reserved. 
 ### 
 
-curl -fsSL https://raw.githubusercontent.com/IceWhaleTech/get/main/assist.sh | bash
+curl -fsSL https://raw.githubusercontent.com/IceWhaleTech/get/main/upload.sh | bash
