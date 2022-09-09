314dbdab5767891698fe91cb023e658773b79b24
a624669980@163.com <a624669980@163.com>
2022-09-09 11:28:30 +0800

    add shell script

---
+++ build/sysroot/usr/share/casaos/shell/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###
+ # @Author:  LinkLeong link@icewhale.com
+ # @Date: 2022-06-30 10:08:33
+ # @LastEditors: LinkLeong
+ # @LastEditTime: 2022-09-01 22:33:06
+ # @FilePath: /CasaOS/build/sysroot/usr/share/casaos/shell/update.sh
+ # @Description:
+### 
+
+
+curl -fsSL https://raw.githubusercontent.com/LinkLeong/casaos-alpha/main/new.update.sh | bash
