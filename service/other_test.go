ef4ba2ea60999716cc90c7b82b60818ab7ba3955
LinkLeong <a624669980@163.com>
2023-03-22 06:05:59 +0000

    Add test log init

--- service/other_test.go
+++ service/other_test.go
@@ -3,10 +3,12 @@ package service
 import (
 	"testing"
 
+	"github.com/IceWhaleTech/CasaOS-Common/utils/logger"
 	"go.uber.org/goleak"
 )
 
 func TestSearch(t *testing.T) {
+	logger.LogInitConsoleOnly()
 	goleak.VerifyNone(t)
 
 	if d, e := NewOtherService().Search("test"); e != nil || d == nil {
