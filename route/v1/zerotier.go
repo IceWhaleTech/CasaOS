78b7c6ce09657e33087239faa20b3cc9e9228d6b
link <a624669980@163.com>
2023-05-23 16:39:25 +0800

    Add zertier (#1116)

--- route/v1/zerotier.go
+++ route/v1/zerotier.go
@@ -6,10 +6,13 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/IceWhaleTech/CasaOS/common"
+	"github.com/IceWhaleTech/CasaOS/pkg/utils/httper"
 	"github.com/gin-gonic/gin"
+	"github.com/tidwall/gjson"
 )
 
-func AddZerotierToken(c *gin.Context) {
+func ZerotierProxy(c *gin.Context) {
 	// Read the port number from the file
 	w := c.Writer
 	r := c.Request
@@ -70,4 +73,137 @@ func copyHeaders(destination, source http.Header) {
 			destination.Add(key, value)
 		}
 	}
+}
+
+func CheckNetwork(c *gin.Context) {
+	//先获取所有已创建的网络
+	respBody, err := httper.ZTGet("/controller/network")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	networkId := ""
+	address := ""
+	networkNames := gjson.ParseBytes(respBody).Array()
+	for _, v := range networkNames {
+		res, err := httper.ZTGet("/controller/network/" + v.Str)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		name := gjson.GetBytes(res, "name").Str
+		if name == common.RANW_NAME {
+			fmt.Println(string(res))
+			networkId = gjson.GetBytes(res, "id").Str
+			break
+		}
+	}
+	if len(networkId) == 0 {
+		if len(address) == 0 {
+			address = GetAddress()
+		}
+		networkId = CreateNet(address)
+	}
+	res, err := httper.ZTGet("/network")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	joined := false
+	networks := gjson.GetBytes(res, "#.id").Array()
+	for _, v := range networks {
+		if v.Str == networkId {
+			fmt.Println("已加入网络")
+			joined = true
+			break
+		}
+	}
+	if !joined {
+		JoinAndUpdateNet(address, networkId)
+	}
+}
+func GetAddress() string {
+	//获取nodeId
+	nodeRes, err := httper.ZTGet("/status")
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return gjson.GetBytes(nodeRes, "address").String()
+}
+func JoinAndUpdateNet(address, networkId string) {
+	res, err := httper.ZTPost("/network/"+networkId, "")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(res))
+	if len(address) == 0 {
+		address = GetAddress()
+	}
+	b := `{
+		"authorized": true,
+		"activeBridge": true,
+		"ipAssignments": [
+		  "10.147.20.1"
+		]
+	  }`
+	r, err := httper.ZTPost("/controller/network/"+networkId+"/member/"+address, b)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(r))
+}
+func CreateNet(address string) string {
+	body := `{
+		"name": "` + common.RANW_NAME + `",
+		"private": false,
+		"v4AssignMode": {
+		"zt": true
+		},
+		"ipAssignmentPools": [
+		{
+		"ipRangeStart": "10.147.20.1",
+		"ipRangeEnd": "10.147.20.254"
+		}
+		],
+		"routes": [
+		{
+		"target": "10.147.20.0/24"
+		}
+		],
+		"rules": [
+		{
+		"etherType": 2048,
+		"not": true,
+		"or": false,
+		"type": "MATCH_ETHERTYPE"
+		},
+		{
+		"etherType": 2054,
+		"not": true,
+		"or": false,
+		"type": "MATCH_ETHERTYPE"
+		},
+		{
+		"etherType": 34525,
+		"not": true,
+		"or": false,
+		"type": "MATCH_ETHERTYPE"
+		},
+		{
+		"type": "ACTION_DROP"
+		},
+		{
+		"type": "ACTION_ACCEPT"
+		}
+		]
+		}`
+	createRes, err := httper.ZTPost("/controller/network/"+address+"______", body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return gjson.GetBytes(createRes, "id").Str
 }
---
+++ route/v2/zerotier.go
@@ -0,0 +1,72 @@
+package v2
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/IceWhaleTech/CasaOS-Common/utils"
+	"github.com/IceWhaleTech/CasaOS/codegen"
+	"github.com/IceWhaleTech/CasaOS/common"
+	"github.com/IceWhaleTech/CasaOS/pkg/utils/httper"
+	"github.com/labstack/echo/v4"
+	"github.com/tidwall/gjson"
+)
+
+func (s *CasaOS) SetZerotierNetworkStatus(ctx echo.Context, networkId string) error {
+	ip := `,"via":"10.147.20.256"`
+	status := ctx.Request().PostFormValue("status")
+	if status == "online" {
+		ip = ``
+	}
+	body := `{
+		"routes": [
+		{
+		"target": "10.147.20.0/24"` + ip + `
+		}
+		]
+		}`
+	res, err := httper.ZTPost("/controller/network/"+networkId, body)
+	if err != nil {
+		fmt.Println(err)
+		return ctx.JSON(http.StatusInternalServerError, codegen.BaseResponse{Message: utils.Ptr(err.Error())})
+	}
+	info := codegen.GetZTInfoOK{}
+	via := gjson.GetBytes(res, "routes.0.via").Str
+	info.Id = utils.Ptr(gjson.GetBytes(res, "id").Str)
+	info.Name = utils.Ptr(gjson.GetBytes(res, "name").Str)
+	if len(via) != 0 {
+		info.Status = utils.Ptr("online")
+	} else {
+		info.Status = utils.Ptr("offline")
+	}
+	return ctx.JSON(http.StatusOK, info)
+}
+func (s *CasaOS) GetZerotierInfo(ctx echo.Context) error {
+	info := codegen.GetZTInfoOK{}
+	respBody, err := httper.ZTGet("/controller/network")
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, codegen.BaseResponse{Message: utils.Ptr(err.Error())})
+	}
+
+	networkNames := gjson.ParseBytes(respBody).Array()
+	for _, v := range networkNames {
+		res, err := httper.ZTGet("/controller/network/" + v.Str)
+		if err != nil {
+			fmt.Println(err)
+			return ctx.JSON(http.StatusInternalServerError, codegen.BaseResponse{Message: utils.Ptr(err.Error())})
+		}
+		name := gjson.GetBytes(res, "name").Str
+		if name == common.RANW_NAME {
+			via := gjson.GetBytes(res, "routes.0.via").Str
+			info.Id = utils.Ptr(gjson.GetBytes(res, "id").Str)
+			info.Name = &name
+			if len(via) != 0 {
+				info.Status = utils.Ptr("online")
+			} else {
+				info.Status = utils.Ptr("offline")
+			}
+			break
+		}
+	}
+	return ctx.JSON(http.StatusOK, info)
+}
